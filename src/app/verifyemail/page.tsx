36fe8d708764b0e95d48d6283ad8a798cd90b957
SnehansuBehera <snehansbehera080903@gmail.com>
2024-04-15 22:37:53 +0530

    few-changes

--- src/app/verifyemail/page.tsx
+++ src/app/verifyemail/page.tsx
@@ -27,7 +27,7 @@ const Verifyemail = () => {
             getVerified();
         }
 
-    }, [token])
+    }, [getVerified])
 
     return (
         <div className='flex flex-col items-center justify-center h-[100vh] bg-black text-white gap-5'>
